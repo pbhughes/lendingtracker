87df329f13d2d3c37fe4e856c53604dc349cf4fe
barkley.hughes@csiweb.com <barkley.hughes@csiweb.com>
2024-11-12 05:52:54 -0600

    got react to start with https = true

--- src/view/lendingview/src/authConfig.js
+++ src/view/lendingview/src/authConfig.js
@@ -42,7 +42,7 @@ export const msalConfig = {
         clientId: '4e63d817-c659-4676-8a6d-d0886466d6db', // This is the ONLY mandatory field that you need to supply.
         authority: b2cPolicies.authorities.signUpSignIn.authority, // Choose SUSI as your default authority.
         knownAuthorities: [b2cPolicies.authorityDomain], // Mark your B2C tenant's domain as trusted.
-        redirectUri: '/', // You must register this URI on Azure Portal/App Registration. Defaults to window.location.origin
+        redirectUri: 'https://localhost:3000', // You must register this URI on Azure Portal/App Registration. Defaults to window.location.origin
         postLogoutRedirectUri: '/', // Indicates the page to navigate after logout.
         navigateToLoginRequestUrl: false, // If "true", will navigate back to the original request location before processing the auth code response.
         scopes: ['openid', 'profile', 'email', 'lender'], // Add scopes as needed for your application.
@@ -86,7 +86,7 @@ export const protectedResources = {
     lenderAPI: {
         endpoint: 'http://localhost:5041/users',
         scopes: {
-            lender: ['https://needthatback.onmicrosoft.com/lender/lender'],
+            lender: ['https://needthatback.onmicrosoft.com/lender'],
         },
     },
 };
