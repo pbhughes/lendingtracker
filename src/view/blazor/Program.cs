c870744c8084129ded960da6033e8e88ae822d94
Barkley Hughes <barkley.hughes@gmail.com>
2024-12-03 18:14:45 -0600

    hung up on net bullshit

--- src/view/blazor/Program.cs
+++ src/view/blazor/Program.cs
@@ -7,6 +7,9 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Microsoft.Graph;
+using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
+
 
 namespace LendingView
 {
@@ -24,6 +27,20 @@ namespace LendingView
                 builder.Configuration.Bind("AzureAdB2C", options.ProviderOptions.Authentication);
                 options.ProviderOptions.AdditionalScopesToConsent.Add("profile");
                 options.ProviderOptions.DefaultAccessTokenScopes.Add("https://needthatback.onmicrosoft.com/lender/lender");
+            });
+
+            // Add Microsoft Graph client
+            builder.Services.AddScoped(sp =>
+            {
+                var accessTokenProvider = sp.GetRequiredService<IAccessTokenProvider>();
+                return new GraphServiceClient(new DelegateAuthenticationProvider(async request =>
+                {
+                    var result = await accessTokenProvider.RequestAccessToken();
+                    if (result.TryGetToken(out var token))
+                    {
+                        request.Headers.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", token.Value);
+                    }
+                }));
             });
 
             await builder.Build().RunAsync();
