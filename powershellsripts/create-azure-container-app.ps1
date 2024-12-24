9d00ec0dae598a94511b9106aeda374979822c6f
pbhughes <barkley.hughes@gmail.com>
2024-12-24 15:03:32 -0600

    working on deployment

---
+++ powershellsripts/create-azure-container-app.ps1
@@ -0,0 +1,28 @@
+﻿# Log in to Azure
+
+
+# Variables
+RESOURCE_GROUP="needthatback"
+LOCATION="westus2"
+ENV_NAME="lendingViewApiContainerEnv"
+APP_NAME="lendingViewApipiContainerApp"
+IMAGE_NAME="ubuntu-latest"
+CONTAINER_PORT=443
+
+# Create a resource group
+az group create --name $RESOURCE_GROUP --location $LOCATION
+
+# Create a container app environment
+az containerapp env create `
+    --name $ENV_NAME `
+    --resource-group $RESOURCE_GROUP `
+    --location $LOCATION
+
+# Create a container app
+az containerapp create `
+    --name $APP_NAME `
+    --resource-group $RESOURCE_GROUP `
+    --environment $ENV_NAME `
+    --image $IMAGE_NAME `
+    --target-port $CONTAINER_PORT `
+    --ingress external
