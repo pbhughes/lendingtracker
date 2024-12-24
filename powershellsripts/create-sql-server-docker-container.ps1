e40f70c9e535ed7abf81eb13d9df62577a9831cc
pbhughes <barkley.hughes@gmail.com>
2024-12-24 17:09:49 -0600

    added docker support

---
+++ powershellsripts/create-sql-server-docker-container.ps1
@@ -0,0 +1,9 @@
+﻿#docker network create lendingviewnetwork
+
+docker run -e "ACCEPT_EULA=Y" `
+   --network lendingviewnetwork `
+   -e "MSSQL_SA_PASSWORD=Reguxalo26@" `
+   -e "MSSQL_PID=Developer" `
+   -p 1433:1433 `
+   --name sqlserver2 `
+   -d mcr.microsoft.com/mssql/server:2022-latest
