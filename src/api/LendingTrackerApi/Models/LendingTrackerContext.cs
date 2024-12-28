6e86f34a04cff4f24b4a522258aa4939f14d7bb5
pbhughes <barkley.hughes@gmail.com>
2024-12-27 20:34:46 -0600

    working online now

--- src/api/LendingTrackerApi/Models/LendingTrackerContext.cs
+++ src/api/LendingTrackerApi/Models/LendingTrackerContext.cs
@@ -29,7 +29,7 @@ public partial class LendingTrackerContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=localhost;Database=LendingTracker;User Id=SA;Password=Reguxalo26@;Connect Timeout=5;TrustServerCertificate=true");
+        => optionsBuilder.UseSqlServer("Name=DefaultConnection");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
