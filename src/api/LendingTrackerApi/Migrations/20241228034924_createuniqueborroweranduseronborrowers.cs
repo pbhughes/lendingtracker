8776cce938a9472e9647c0f6d5845d014781f233
pbhughes <barkley.hughes@gmail.com>
2024-12-27 22:12:38 -0600

    added constraint for uniqu userid and borrower email

---
+++ src/api/LendingTrackerApi/Migrations/20241228034924_createuniqueborroweranduseronborrowers.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LendingTrackerApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class createuniqueborroweranduseronborrowers : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(name: "UQ__Borrower__14F16AF6B87CB05D", table: "Borrowers");
+
+            migrationBuilder.CreateIndex(name: "UQ__Borrower__User__14F16AF6B87CB05D", table: "Borrowers", columns: new[] { "BorrowerEmail", "UserId" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(name: "UQ__Borrower__User__14F16AF6B87CB05D");
+
+            migrationBuilder.CreateIndex(name: "UQ__Borrower__14F16AF6B87CB05D", table: "Borrowers", columns: new [] { "BorrowerEmail" });
+        }
+    }
+}
