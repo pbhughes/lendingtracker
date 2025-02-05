68f2fa419173e75383cc812ea8ada0bbf1c2ce33
barkley.hughes@csiweb.com <barkley.hughes@csiweb.com>
2025-02-05 14:34:55 -0600

    getting store link worked out

---
+++ src/api/LendingTrackerApi/Migrations/20250205203103_nullableagainagain.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LendingTrackerApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class nullableagainagain : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+            name: "StoreLink",
+            table: "Items",
+            type: "nvarchar(max)",
+            nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+            name: "StoreLink",
+            table: "Items",
+            type: "nvarchar(max)",
+            nullable: false);
+        }
+    }
+}
