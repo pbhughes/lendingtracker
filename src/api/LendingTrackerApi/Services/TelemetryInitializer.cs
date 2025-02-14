4f8eb4df969eed87fe7a7948b9addf3b22843503
pbhughes <barkley.hughes@gmail.com>
2025-02-14 17:52:09 -0600

    added app insights to api

---
+++ src/api/LendingTrackerApi/Services/TelemetryInitializer.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.ApplicationInsights.Channel;
+using Microsoft.ApplicationInsights.Extensibility;
+
+namespace LendingTrackerApi.Services
+{
+    public class TelemetryInitializer : ITelemetryInitializer
+    {
+       private readonly IConfiguration _configuration;
+
+        public TelemetryInitializer(IConfiguration config)
+        {
+            _configuration = config;
+        }
+        public void Initialize(ITelemetry telemetry)
+        {
+            if (string.IsNullOrEmpty(telemetry.Context.Cloud.RoleName))
+            {
+                telemetry.Context.Cloud.RoleName = $"{_configuration["ApplicationInsights:CloudRoleName"]}";
+            }
+        }
+    }
+}
