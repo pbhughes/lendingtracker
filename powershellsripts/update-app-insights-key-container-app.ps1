f20c0fcc6a2502cc5d353adb462c65ca0e9bd700
pbhughes <barkley.hughes@gmail.com>
2025-02-15 08:05:27 -0600

    added app insights to both client and API

---
+++ powershellsripts/update-app-insights-key-container-app.ps1
@@ -0,0 +1,4 @@
+﻿az containerapp update `
+  --name api-lending-tracker `
+  --resource-group needthatback `
+  --set-env-vars APPLICATIONINSIGHTS_CONNECTION_STRING="InstrumentationKey=34e61473-fb29-43a9-90c6-b5324284ff96;IngestionEndpoint=https://westus2-2.in.applicationinsights.azure.com/;LiveEndpoint=https://westus2.livediagnostics.monitor.azure.com/;ApplicationId=a1daa383-6400-4e5b-828b-c5ebd3060666"
--- src/view/blazor/Program.cs
+++ src/view/blazor/Program.cs
@@ -1,3 +1,4 @@
+using BlazorApplicationInsights;
 using Blazored.LocalStorage;
 using LendingView.Servcies;
 using Microsoft.AspNetCore.Components.Web;
@@ -36,6 +37,11 @@ namespace LendingView
                 options.ProviderOptions.LoginMode = "redirect";
             });
 
+            //add observability
+            builder.Services.AddBlazorApplicationInsights(x =>
+            {
+                x.ConnectionString = "InstrumentationKey=34e61473-fb29-43a9-90c6-b5324284ff96;IngestionEndpoint=https://westus2-2.in.applicationinsights.azure.com/;LiveEndpoint=https://westus2.livediagnostics.monitor.azure.com/;ApplicationId=a1daa383-6400-4e5b-828b-c5ebd3060666";
+            });
 
             //support authorization
             builder.Services.AddAuthorizationCore(options =>
