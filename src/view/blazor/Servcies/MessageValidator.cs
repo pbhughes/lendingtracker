b085f29d0e1ac00f4920b65ff960e422e6c31613
pbhughes <barkley.hughes@gmail.com>
2025-03-09 19:03:26 -0500

    Working messaging

---
+++ src/view/blazor/Servcies/MessageValidator.cs
@@ -0,0 +1,22 @@
+﻿using FluentValidation;
+
+namespace LendingView.Servcies
+{
+    public class MessageValidator<T> : AbstractValidator<T>
+    {
+        public MessageValidator(Action<IRuleBuilderInitial<T,T>> rule)
+        {
+            rule(RuleFor(x => x));
+        }
+
+        private IEnumerable<string> ValidateValue(T arg)
+        {
+            var result = Validate(arg);
+            if (result.IsValid)
+                return new string[0];
+            return result.Errors.Select(e => e.ErrorMessage);
+        }
+
+        public Func<T, IEnumerable<string>> Validation => ValidateValue;
+    }
+}
